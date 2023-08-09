495c7af6e72fa1ff742478b42883ff426e47f3bb
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-08-09 19:14:00 +0400

    feat: add base calculator operations
    Implement add, subtract, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+
+function subtract(a, b) {
+  return a - b;
+}
+
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(add(15, 5));
+console.log(subtract(15, 5));
+console.log(multiply(15, 5));
+console.log(divide(15, 5));
