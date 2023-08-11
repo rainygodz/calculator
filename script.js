e1e1e5d51f67733a61b4a2ecbe220956612c1d0f
rainygodz <120272656+rainygodz@users.noreply.github.com>
2023-08-11 17:42:07 +0400

    feat: add evaluate function
    Function calculates result of 2 numbers operation

--- script.js
+++ script.js
@@ -24,24 +24,61 @@ function operate(operator, operand1, operand2) {
       return add(operand1, operand2);
     case '-':
       return subtract(operand1, operand2);
-    case '*':
+    case '×':
       return multiply(operand1, operand2);
-    case '/':
+    case '÷':
       return divide(operand1, operand2);
   }
 }
 
 
 function updateDisplay(event) {
-  if (event.target.id === "C") {
-    return;
+
+  switch(event.target.id) {
+    case "add":
+      display.textContent += " + ";
+      break;
+    case "subtract":
+      display.textContent += " - ";
+      break;
+    case "multiply":
+      display.textContent += " × ";
+      break;
+    case "divide":
+      display.textContent += " ÷ ";
+      break;
+    case "equals":
+      display.textContent += "";
+      break;
+    case "C":
+      display.textContent += "";
+      break;
+    default:
+      display.textContent += event.target.id;
+      break;
   }
-  display.textContent += event.target.id;
+}
+
+
+function storeVariables() {
+  const expression = display.textContent;
+  const expressionParts = expression.split(" ");
+  operand1 = parseInt(expressionParts[0]);
+  operator = expressionParts[1];
+  operand2 = parseInt(expressionParts[2]);
+}
+
+
+function evaluate() {
+  storeVariables();
+  const result = operate(operator, operand1, operand2);
+  display.textContent = result;
 }
 
 
 const display = document.querySelector(".display-content");
 const buttons = document.querySelectorAll(".button");
+let displayContent = "";
 
 buttons.forEach(button => {
   button.addEventListener("click", updateDisplay);
@@ -49,3 +86,8 @@ buttons.forEach(button => {
 
 const realButton = document.querySelector("button");
 realButton.addEventListener("click", updateDisplay);
+
+
+const equalsButton = document.querySelector("#equals");
+
+equalsButton.addEventListener("click", evaluate);
